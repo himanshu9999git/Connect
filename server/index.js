fb16ce12c479133abf5582dd2a1760960a38783f
himanshu9999git <hchimanshu9999@gmail.com>
2023-02-20 15:07:19 +0530

    Get User, UserFriends, AddRemoveFriends

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -40,6 +41,8 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* Routes */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
 
 /* Mongooose SetUp */
 const PORT = process.env.PORT || 6001;
