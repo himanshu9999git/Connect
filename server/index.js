0c8de2da858c082e08a6ff8470b61431b85d0deb
himanshu9999git <hchimanshu9999@gmail.com>
2023-02-20 15:14:24 +0530

    Create, Like Posts And Get Feed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js";
+import userRoutes from "./routes/users.js";import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,11 +41,12 @@ const upload = multer({ storage });
 
 /* Routes With Files */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* Routes */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-
+app.use("/posts", postRoutes);
 
 /* Mongooose SetUp */
 const PORT = process.env.PORT || 6001;
