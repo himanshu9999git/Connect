f99d5448960a20d64bb4894e7a7a56e359c8b694
himanshu9999git <hchimanshu9999@gmail.com>
2023-02-20 15:54:41 +0530

    Added Sample Data (Backend is Complete)

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ import userRoutes from "./routes/users.js";import postRoutes from "./routes/post
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -59,5 +62,11 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // Add data one time
+    // Comment after adding to avoid duplicate data
+
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
